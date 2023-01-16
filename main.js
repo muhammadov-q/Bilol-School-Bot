22ffe69667a464b345f172231c00d0b2a30849c4
Takhirzhon <tashmatovtahir@gmail.com>
2023-01-16 21:19:42 +0600

    Working on loop

--- main.js
+++ main.js
@@ -13,16 +13,17 @@ addEventListener("fetch", event => {
         // Checking if the payload comes from Telegram
         const chatId = payload.message.chat.id
 
-        var qobiljon = 0;
-        var takhir = 0;
-        var erkebai = 0;
+        // var qobiljon = 0;
+        // var takhir = 0;
+        // var erkebai = 0;
+        var counter = 0;
 
         switch(payload.message.text) {
-          case "Qobiljon": qobiljon++; break;
-          case "Takhir": takhir++; break; 
-          case "Erkebai": erkebai++; break;
+          case "Qobiljon": counter++; break;
+          case "Takhir": counter++; break; 
+          case "Erkebai": counter++; break;
           case "Owner": 
-          const url = `https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${takhir + " " + erkebai + " " + qobiljon}`;
+          const url = `https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${counter}`;
           const data = await fetch(url).then(resp => resp.json());
           // Calling the API endpoint to send a telegram message
           break;
