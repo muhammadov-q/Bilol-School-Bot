b77419bed62a8d0b2d23329a0436dc6ebe47ebd2
Takhirzhon <tashmatovtahir@gmail.com>
2023-01-16 22:48:49 +0600

    Rewriting code

--- main.js
+++ main.js
@@ -7,29 +7,26 @@ addEventListener("fetch", event => {
   
   async function handleRequest(request) {
     if (request.method === "POST") {
-      var payload = await request.json() 
+      const payload = await request.json() 
       // Getting the POST request JSON payload
       if ('message' in payload) { 
         // Checking if the payload comes from Telegram
-        var chatId = payload.message.chat.id
+        const chatId = payload.message.chat.id
 
+        // var qobiljon = 0;
+        // var takhir = 0;
+        // var erkebai = 0;
         var counter = 0;
-        var url;
-        var data;
 
         switch(payload.message.text) {
-          case "Qobiljon": 
-          case "Takhir": 
+          case "Qobiljon": counter++; break;
+          case "Takhir": counter++; break; 
           case "Erkebai": counter++; break;
-          case "owner": fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${counter}`); break;
-          default: fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${"Incorrect input"}`); break;
-        }
-
-        if (payload.message.text == "Owner")
-        {
-          url = `https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${counter}`;
-          data = await fetch(url).then(resp => resp.json());
+          case "Owner": 
+          const url = `https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${counter}`;
+          const data = await fetch(url).then(resp => resp.json());
           // Calling the API endpoint to send a telegram message
+          break;
         }
       }
     }
