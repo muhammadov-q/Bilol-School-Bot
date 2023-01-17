45e512892d282d0000b0be0ff999f806dfc7ffca
muhammadov-q <muhammadov_q@auca.kg>
2023-01-17 10:34:52 +0600

    Woking

--- main.js
+++ main.js
@@ -1,42 +1,35 @@
 addEventListener("fetch", event => {
   event.respondWith(handleRequest(event.request))
 })
+
 async function handleRequest(request) {
   return new Response("Hello world")
 }
 
+let count = 0;
 
 async function handleRequest(request) {
-    if (request.method === "POST") {
-      var payload = await request.json() 
-      if ('message' in payload) { 
-            
-      if (text === '/info') {
-        await payload.sendMessage(chatId,'Your name is '  + message.from.first_name + ' ' + message.from.last_name)
-      }
-      // Getting the POST request JSON payload
-      
-      // Checking if the payload comes from Telegram
-
-        var counter = 0;
-        var url;
-        var data;
-
-        switch(payload.message.text) {
-          case "Qobiljon": 
-          case "Takhir": 
-          case "Erkebai": counter++; break;
-          case "owner": fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${counter++}`); break;
-          default: fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${"Incorrect input"}`); break;
-        }
-
-        if (payload.message.text == "Owner")
-        {
-          url = `https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${counter}`;
-          data = await fetch(url).then(resp => resp.json());
-          // Calling the API endpoint to send a telegram message
-        }
-      }
+  if (request.method === "POST") {
+    let payload = await request.json() // Getting the POST request JSON payload
+    let chatId = payload.message.chat.id
+    
+    if (payload.message.text == "abs")
+    {
+      count++;
     }
-    return new Response("OK") // Doesn't really matter
+
+    let text = payload.message.text + " over " + count;
+
+    if (payload.message.text == "owner")
+    {   
+    let url = `https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${text}`;
+    let data = await fetch(url).then(resp => resp.json()) // Calling the API endpoint to send a telegram message
+    }
+
+    if (payload.message.text == "clear")
+    {
+      count = 0;
+    }
   }
+  return new Response("OK") // Doesn't really matter
+}
