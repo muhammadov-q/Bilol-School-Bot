c2c2e381f2fc235342bfdc7db46508e00d43035a
muhammadov-q <muhammadov_q@auca.kg>
2023-01-20 22:37:36 +0600

    Project is Finished!

--- src/main.js
+++ src/main.js
@@ -17,50 +17,51 @@ async function handleRequest(request) {
       hour -= 23;
     }
     
-    if (hour >= 14 && hour <= 20) {
-    switch (payload.message.text)
+    if (hour >= 0 && hour <= 23) {
+    switch ((payload.message.text).toLowerCase())
     { 
-      case "a": 
+      case "professor-a": 
         a++;
-        fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${"Congrats Dear a! You registerd on time ✅"}`);
+        fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${"Tabriklaymiz hurmatli professor-a! Siz o'z vaqtida ro'yxatga olindiz ✅"}`);
         break;
-      case "b": 
+      case "professor-b": 
         b++;
-        fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${"Congrats Dear b! You registerd on time ✅"}`);
+        fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${"Tabriklaymiz hurmatli professor-b! Siz o'z vaqtida ro'yxatga olindiz ✅"}`);
         break; 
-      case "c": 
+      case "professor-c": 
         c++;
-        fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${"Congrats Dear c! You registerd on time ✅"}`);
+        fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${"Tabriklaymiz hurmatli professor-c! Siz o'z vaqtida ro'yxatga olindiz ✅"}`);
         break;
-        case "/start":
-          fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${"Welcome to my bot! Type /info for more information."}`);
-          break;
-      
+      case "/start":
+        fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${"Bilol School online davomat sistemasiga xush kelibsiz! Qo'llash uchun ma'lumotni /info bo'limidan topishingiz mumkin."}`);
+        break;
+      case "/info":
+        fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${"Bot quydagi tartibda ishlaydi: siz faqatgina belgilangan vaqtdagina ya'ni 8.35 dan 8.45 ga qadar ro'yxat olinadi. Sizdan ismingizni lotin alifbosida yozish talab qilinadi. Siz ushbu xabarni qabul qilsangiz <Tabriklaymiz hurmatli |ismingiz|! Siz o'z vaqtida ro'yxatga olindiz ✅>, demak siz ro'yxatga olindiz. Aks holda kech qoldingiz yoki yana bir bora qaytib yozing!"}`);
+        break;
       default:
-        if (!(payload.message.text == "clear" || payload.message.text == "owner")) 
+        if (!((payload.message.text).toLowerCase() == "tozalash" || (payload.message.text).toLowerCase() == "rajabov")) 
         {
-             fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${"Invalid UserName 🧐"}`); 
-        }
-        break;   
+             fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${"Ismda xatolik! Iltimos qayta urinib ko'ring 🧐"}`); 
+        }   
     }
-  } else if (!(payload.message.text == "clear" || payload.message.text == "owner")) {
-        fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${"Registration time has passed ☹️"}`);
+  } else if (!((payload.message.text).toLowerCase() == "tozalash" || (payload.message.text).toLowerCase() == "rajabov")) {
+        fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${"Ro'yxatga olish vaqti o'tib ketdi! Afsus ☹️"}`);
     }
     
-    if (payload.message.text == "clear")
+    if ((payload.message.text).toLowerCase() == "tozalash")
     {
       a = b = c = 0;
-      fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${"Wohoo 🥳. You cleared data succesfully! 🗑"}`);
+      fetch(`https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${"Wohoo 🥳. Siz ma'lumotlarni muvoffaqiyatli tozaladingiz! 🗑"}`);
     }
 
     let statusA, statusB, statusC;
-    statusA = (a >= 1) ? "Present ✅" : "Absent ❌";
-    statusB = (b >= 1) ? "Present ✅" : "Absent ❌";
-    statusC = (c >= 1) ? "Present ✅" : "Absent ❌";
+    statusA = (a >= 1) ? "Mavjud ✅" : "Mavjud emas ❌";
+    statusB = (b >= 1) ? "Mavjud ✅" : "Mavjud emas ❌";
+    statusC = (c >= 1) ? "Mavjud ✅" : "Mavjud emas ❌";
 
-    let text = " a --- " + statusA + "\n\nb --- " + statusB + "\n\nc --- " + statusC;
+    let text = " ProfessorA --- " + statusA + "\n\nProfessorB --- " + statusB + "\n\nProfeesorC --- " + statusC;
 
-    if (payload.message.text == "owner")
+    if ((payload.message.text).toLowerCase() == "rajabov")
     {   
       let url = `https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${text}`;
       let data = await fetch(url).then(resp => resp.json()) // Calling the API endpoint to send a telegram message
